e657d161e8a6b5f9e310d3fc465a995752e3ef67
Muhammad Athma Farhan <farhan.athma@gmail.com>
2025-08-14 00:41:15 +0700

    course number 347. wild oasis react query setup

---
+++ 17-the-wild-oasis/final-1-after-setup/src/services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://mtwrmcrlfaadzahjbfpm.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10d3JtY3JsZmFhZHphaGpiZnBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0NzEwNDIsImV4cCI6MjA3MDA0NzA0Mn0.D7O_-8XDr4G8s3rQ-X2MjGCjROyvXwj7v7d6EhCk41s'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
