12a9e1d3ee23709c3924f6b16137d74bfc8fe5d8
Muhammad Athma Farhan <farhan.athma@gmail.com>
2025-08-26 00:16:19 +0700

    368. Converting the Modal to a Compound Component

--- 17-the-wild-oasis/final-3-after-react-query/src/services/supabase.js
+++ 17-the-wild-oasis/final-3-after-react-query/src/services/supabase.js
@@ -1,8 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
+export const supabaseUrl = 'https://mtwrmcrlfaadzahjbfpm.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10d3JtY3JsZmFhZHphaGpiZnBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0NzEwNDIsImV4cCI6MjA3MDA0NzA0Mn0.D7O_-8XDr4G8s3rQ-X2MjGCjROyvXwj7v7d6EhCk41s'
+const supabase = createClient(supabaseUrl, supabaseKey)
 
-export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
+export default supabase
